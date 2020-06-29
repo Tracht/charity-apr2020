921ca0cb23f7b84bab7b608628ab42fdb83592f4
Catriona <catrionamairi@blueyonder.co.uk>
2020-06-29 16:43:02 +0100

    Add delete button for drivers

--- public/javascripts/components/Driver.component.js
+++ public/javascripts/components/Driver.component.js
@@ -1,9 +1,12 @@
 function Driver (props) {
   return(
-    <tr>
-      <td>{props._id}</td>
-      <td className="driverName">{props.name}</td>
-      <td className="driverAddress">{props.address}</td>
-    </tr>
+    <div>
+      <tr>
+        <td>{props._id}</td>
+        <td className="driverName">{props.name}</td>
+        <td className="driverAddress">{props.address}</td>
+      </tr>
+      <button id="delete" value="delete">delete</button>
+    </div>
   );
 }
