c8ac3e05e512d402c69ba2a5174ad1780eea55a0
tanilserbes <serbestanil@gmail.com>
2020-06-29 17:07:29 +0100

    Add delete function to the component

--- public/javascripts/components/Driverlist.component.js
+++ public/javascripts/components/Driverlist.component.js
@@ -1,40 +1,65 @@
 class DriverList extends React.Component {
-
   constructor() {
     super();
     this.state = {
       drivers: [],
     };
-  };
+  }
 
   componentDidMount() {
-    this.fetchDrivers('/drivers');
+    this.fetchDrivers("/drivers");
   }
 
   fetchDrivers = () => {
-    fetch('/drivers')
-    .then(response => response.json())
-    .then((data) => {
-      this.setState({
-        drivers: data,
+    fetch("/drivers")
+      .then((response) => response.json())
+      .then((data) => {
+        this.setState({
+          drivers: data,
+        });
+        this.setState({
+          drivers: this.sortDriversAtoZ(),
+        });
       });
-      this.setState({
-        drivers: this.sortDriversAtoZ(),
-      });
-      
-    });
-  }
+  };
 
   sortDriversAtoZ = () => {
-    return this.state.drivers.sort(function(memberA, memberB) {
-    var memberA = memberA.name.toUpperCase();
-    var memberB = memberB.name.toUpperCase();
-      return (memberA < memberB) ? -1 : (memberA > memberB) ? 1 : 0;
+    return this.state.drivers.sort(function (memberA, memberB) {
+      var memberA = memberA.name.toUpperCase();
+      var memberB = memberB.name.toUpperCase();
+      return memberA < memberB ? -1 : memberA > memberB ? 1 : 0;
     });
-  }
+  };
+
+  delete = (e) => {
+    console.log(document.getElementById("delete").value);
+    // e.preventDefault();
+    // const driverid = {
+    //   driverid: d
+    //   password: this.state.password,
+    // };
+    // console.log(admin);
+
+    // fetch("/delete", {
+    //   method: "post",
+    //   headers: { "Content-Type": "application/json" },
+    //   body: JSON.stringify(admin),
+    // })
+    //   .then((response) => response.json())
+    //   .then((data) => {
+    //     console.log(data);
+    //     if (data.success === true) {
+    //       sessionStorage.setItem("token", data.admin._id);
+    //       console.log(sessionStorage);
+    //       window.location.replace("/");
+    //     } else {
+    //       alert(`${data.message}`);
+    //     }
+    //   });
+  };
 
   render() {
-    return(
+    return (
       <div>
         <h2>List of Drivers</h2>
         <table className="driver-list">
@@ -47,12 +72,25 @@ class DriverList extends React.Component {
           </thead>
           <tbody>
             {this.state.drivers.map((driver) => (
-              < Driver name={driver.name} address={driver.address} id={driver._id} key={driver._id} />
-              ))
-            }
+              <div>
+                <button
+                  id={driver._id}
+                  onClick={this.delete}
+                  value={driver._id}
+                >
+                  delete
+                </button>
+                <Driver
+                  name={driver.name}
+                  address={driver.address}
+                  id={driver._id}
+                  key={driver._id}
+                />
+              </div>
+            ))}
           </tbody>
         </table>
       </div>
-    )
+    );
   }
 }
