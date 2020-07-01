b4292d70f1259c4b54418417f39c3512b5435e6f
tanilserbes <serbestanil@gmail.com>
2020-07-01 12:36:25 +0100

    Update

--- controllers/homepage.js
+++ controllers/homepage.js
@@ -1,27 +1,29 @@
 var HomepageController = {
-
   CreateMember: (memberModel) => (request, response) => {
-
     var member = new memberModel({
       name: request.body.name,
       role: request.body.role,
       address: request.body.address,
-      availability: { },
+      availability: {},
     });
 
-
     member.save((err) => {
-      if (err) { console.log(err) }
+      if (err) {
+        console.log(err);
+      }
       // sendFlashMessage(response, request, '/', 'Member saved!');
-      response.send({message: "ok"});
+      response.send({ message: "ok" });
     });
   },
 
   DeleteMember: (memberModel) => (request, response) => {
-    console.log("we are in delete member function")
+    console.log("we are in delete member function");
+    console.log(request);
     var id = request.body.id;
-    memberModel.findByIdAndRemove({"_id" : id}, (err) => {
-      if(err) { throw err; }
+    memberModel.delete({ _id: id }, (err) => {
+      if (err) {
+        throw err;
+      }
     });
   },
 
@@ -30,8 +32,7 @@ var HomepageController = {
 
     memberModel.find((err, result) => {
       result.forEach((member) => {
-
-        if(member.role === 'driver'){
+        if (member.role === "driver") {
           drivers.push(member);
         }
       });
@@ -44,16 +45,14 @@ var HomepageController = {
 
     memberModel.find((err, result) => {
       result.forEach((member) => {
-
-        if(member.role === 'guest'){
+        if (member.role === "guest") {
           guests.push(member);
         }
       });
       response.send(guests);
     });
-  }
-
-}
+  },
+};
 
 // var sendFlashMessage = (response, request, route, message) => {
 //   request.session.errorMessage = message;
